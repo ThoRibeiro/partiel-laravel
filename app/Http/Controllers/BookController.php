2246c74a4a6629c8f2d2a991c5c29c59e211964e
Ribeiro Thomas <tribeiro@auchan.fr>
2024-07-25 16:15:28 +0200

    feat: add controller book

---
+++ app/Http/Controllers/BookController.php
@@ -0,0 +1,90 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Book;
+use Illuminate\Http\Request;
+
+class BookController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $books = Book::all();
+        return view('books.index', compact('books'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        return view('books.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $validatedData = $request->validate([
+            'title' => 'required|string|max:255',
+            'author' => 'required|string|max:255',
+            'year' => 'required|integer',
+            'genre' => 'nullable|string|max:255',
+        ]);
+
+        Book::create($validatedData);
+
+        return redirect()->route('books.index')->with('success', 'Livre ajouté avec succès.');
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        $book = Book::findOrFail($id);
+        return view('books.show', compact('book'));
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(string $id)
+    {
+        $book = Book::findOrFail($id);
+        return view('books.edit', compact('book'));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        $validatedData = $request->validate([
+            'title' => 'required|string|max:255',
+            'author' => 'required|string|max:255',
+            'year' => 'required|integer',
+            'genre' => 'nullable|string|max:255',
+        ]);
+
+        Book::whereId($id)->update($validatedData);
+
+        return redirect()->route('books.index')->with('success', 'Livre mis à jour avec succès.');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        $book = Book::findOrFail($id);
+        $book->delete();
+
+        return redirect()->route('books.index')->with('success', 'Livre supprimé avec succès.');
+    }
+
+}
--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,8 @@
 use App\Http\Controllers\BookController;
 use Illuminate\Support\Facades\Route;
 
+Route::get('/', [BookController::class, 'index'])->name('home');
+
 Route::get('/books', [BookController::class, 'index'])->name('books.index');
 
 Route::get('/books/create', [BookController::class, 'create'])->name('books.create');
