05ca2dad08a2d03197c102cfea1fcf06b12f381e
Ribeiro Thomas <tribeiro@auchan.fr>
2024-07-25 16:10:12 +0200

    fix: routes

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use App\Http\Controllers\BookController;
-use Illuminate\Routing\Route;
+use Illuminate\Support\Facades\Route;
 
 Route::get('/books', [BookController::class, 'index'])->name('books.index');
 
@@ -12,9 +12,7 @@ Route::post('/books', [BookController::class, 'store'])->name('books.store');
 Route::get('/books/{id}/edit', [BookController::class, 'edit'])->name('books.edit');
 
 Route::put('/books/{id}', [BookController::class, 'update'])->name('books.update');
-Route::patch('/books/{id}', [BookController::class, 'update'])->name('books.update');
 
 Route::delete('/books/{id}', [BookController::class, 'destroy'])->name('books.destroy');
-
 
 ?>
